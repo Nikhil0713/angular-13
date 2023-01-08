cc59bae43030e3b43ac6be29b6cc629d6fb60d54
Nikhil0713 <nikhilguggilla171@gmail.com>
2023-01-09 00:54:07 +0530

    first commit

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form: any = {
+    firstname : null,
+      lastname: null,
+      email: null,
+      location: null,
+      jobtitle: null,
+      department: null,
+      role: null,
+      password: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log("entered inti onsubmit")
+    const {firstname,lastname,email,location,jobtitle,department,role,password} = this.form;
+
+    this.authService.register(firstname,lastname,email,location,jobtitle,department,role,password).subscribe({
+      next: data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    });
+  }
+}
